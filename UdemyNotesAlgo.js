0e93850acce4db71a174b1af673a72ddda64d0c9
grantkoji <grantkoji@gmail.com>
2020-10-20 23:42:19 -0400

    with more changes

--- UdemyNotesAlgo.js
+++ UdemyNotesAlgo.js
@@ -470,3 +470,71 @@ areThereDuplicates One Liner Solution
 function areThereDuplicates() {
   return new Set(arguments).size !== arguments.length;
 }
+
+function averagePair(arr, avg){
+    // add whatever parameters you deem necessary - good luck!
+    let start = 0;
+    let end = arr.length - 1;
+    while(start < end){
+        let thisAvg = (arr[start] + arr[end])/2
+      if(thisAvg === avg){
+          return true
+      } else if (thisAvg < avg){
+          start++
+      } else {
+          end--
+      }
+    }
+    
+  
+    return false
+  }
+
+  averagePair Solution
+function averagePair(arr, num){
+  let start = 0
+  let end = arr.length-1;
+  while(start < end){
+    let avg = (arr[start]+arr[end]) / 2 
+    if(avg === num) return true;
+    else if(avg < num) start++
+    else end--
+  }
+  return false;
+}
+isSubsequence Solution - Iterative
+function isSubsequence(str1, str2) {
+  var i = 0;
+  var j = 0;
+  if (!str1) return true;
+  while (j < str2.length) {
+    if (str2[j] === str1[i]) i++;
+    if (i === str1.length) return true;
+    j++;
+  }
+  return false;
+}
+isSubsequence Solution - Recursive but not O(1) Space
+function isSubsequence(str1, str2) {
+  if(str1.length === 0) return true
+  if(str2.length === 0) return false
+  if(str2[0] === str1[0]) return isSubsequence(str1.slice(1), str2.slice(1))  
+  return isSubsequence(str1, str2.slice(1))
+}
+
+function isSubsequence(str1, str2) {
+    // good luck. Add any arguments you deem necessary.
+    let i1 = 0
+    let j2 = 0
+    if (!str1) return true;
+    while (j2 < str2.length) {
+        if(str1[i1] === str2[j2]) i1++;
+        if(i1 === str1.length) return true;
+        j2++
+    }
+    return false
+        
+   
+    
+  }
+
