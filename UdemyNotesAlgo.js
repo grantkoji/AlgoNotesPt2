e5188a95956bc7b54a0b31ce40fd288fcccf2517
grantkoji <grantkoji@gmail.com>
2020-10-29 11:22:54 -0400

    with challenging recursion problems

--- UdemyNotesAlgo.js
+++ UdemyNotesAlgo.js
@@ -978,4 +978,185 @@ function reverse(str){
 	return reverse(str.slice(1)) + str[0];
 }
 
+
+function capitalizeFirst (arr) {
+    // add whatever parameters you deem necessary - good luck!
+    let newArr = []
+    function capArr(array){
+        if (array.length === 0){
+            return;
+        }
+        newArr.push(array[0].charAt(0).toUpperCase()+array[0].slice(1))
+        capArr(array.slice(1))
+    }
+    capArr(arr)
+    return newArr
+  }
+  
+  // capitalizeFirst(['car','taco','banana']); // ['Car','Taco','Banana']
+  function capitalizeWords (array) {
+    if (array.length === 1) {
+      return [array[0].toUpperCase()];
+    }
+    let res = capitalizeWords(array.slice(0, -1));
+    res.push(array.slice(array.length-1)[0].toUpperCase());
+    return res;
+   
+  }
+
+  function capitalizeWords (arr) {
+    // add whatever parameters you deem necessary - good luck!
+    let newArr=[]
+    function capArray(array){
+        if (array.length === 0){
+            return;
+        }
+        newArr.push(array[0].toUpperCase())
+        capArray(array.slice(1))
+    }
+    
+    
+    capArray(arr)
+    return newArr
+  }
+  
+  // let words = ['i', 'am', 'learning', 'recursion'];
+  // capitalizedWords(words); // ['I', 'AM', 'LEARNING', 'RECURSION']
+
+  stringifyNumbers Solution
+
+  function stringifyNumbers(obj) {
+    var newObj = {};
+    for (var key in obj) {
+      if (typeof obj[key] === 'number') {
+        newObj[key] = obj[key].toString();
+      } else if (typeof obj[key] === 'object' && !Array.isArray(obj[key])) {
+        newObj[key] = stringifyNumbers(obj[key]);
+      } else {
+        newObj[key] = obj[key];
+      }
+    }
+    return newObj;
+  }
+
+  The Array.isArray() method determines whether the passed value is an Array.
+  function collectStrings(obj){
+    let arrayString = []
+    for (var key in obj){
+        if(typeof obj[key] === 'string'){
+            arrayString.push(obj[key])
+        } else if (typeof obj[key] === 'object' && !Array.isArray(obj[key])){
+             arrayString = arrayString.concat(collectStrings(obj[key]));
+        }
+        
+    }
+    return arrayString
+    
+}
+
+
+const obj = {
+    stuff: "foo",
+    data: {
+        val: {
+            thing: {
+                info: "bar",
+                moreInfo: {
+                    evenMoreInfo: {
+                        weMadeIt: "baz"
+                    }
+                }
+            }
+        }
+    }
+}
+
+collectStrings(obj) // ["foo", "bar", "baz"])
+
+capitalizeWords Solution
+
+function capitalizeWords (array) {
+  if (array.length === 1) {
+    return [array[0].toUpperCase()];
+  }
+  let res = capitalizeWords(array.slice(0, -1));
+  res.push(array.slice(array.length-1)[0].toUpperCase());
+  return res;
+ 
+}
+nestedEvenSum Solution
+
+function nestedEvenSum (obj, sum=0) {
+    for (var key in obj) {
+        if (typeof obj[key] === 'object'){
+            sum += nestedEvenSum(obj[key]);
+        } else if (typeof obj[key] === 'number' && obj[key] % 2 === 0){
+            sum += obj[key];
+        }
+    }
+    return sum;
+}
+capitalizeFire Solution
+
+function capitalizeFirst (array) {
+  if (array.length === 1) {
+    return [array[0][0].toUpperCase() + array[0].substr(1)];
+  }
+  const res = capitalizeFirst(array.slice(0, -1));
+  const string = array.slice(array.length - 1)[0][0].toUpperCase() + array.slice(array.length-1)[0].substr(1);
+  res.push(string);
+  return res;
+}
+stringifyNumbers Solution
+
+function stringifyNumbers(obj) {
+  var newObj = {};
+  for (var key in obj) {
+    if (typeof obj[key] === 'number') {
+      newObj[key] = obj[key].toString();
+    } else if (typeof obj[key] === 'object' && !Array.isArray(obj[key])) {
+      newObj[key] = stringifyNumbers(obj[key]);
+    } else {
+      newObj[key] = obj[key];
+    }
+  }
+  return newObj;
+}
+collectStrings Solution: Helper Method Recursion Version
+
+function collectStrings(obj) {
+    var stringsArr = [];
+ 
+    function gatherStrings(o) {
+        for(var key in o) {
+            if(typeof o[key] === 'string') {
+                stringsArr.push(o[key]);
+            }
+            else if(typeof o[key] === 'object') {
+                return gatherStrings(o[key]);
+            }
+        }
+    }
+ 
+    gatherStrings(obj);
+ 
+    return stringsArr;
+}
+collectStrings Solution: Pure Recursion Version
+
+function collectStrings(obj) {
+    var stringsArr = [];
+    for(var key in obj) {
+        if(typeof obj[key] === 'string') {
+            stringsArr.push(obj[key]);
+        }
+        else if(typeof obj[key] === 'object') {
+            stringsArr = stringsArr.concat(collectStrings(obj[key]));
+        }
+    }
+ 
+    return stringsArr;
+}
+
+
 ljdfjakldja;jf
