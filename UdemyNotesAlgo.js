d83e25c9af047f283b34aca7a8fecdb31215ac11
grantkoji <grantkoji@gmail.com>
2020-11-13 12:20:02 -0500

    with binary search tree

--- UdemyNotesAlgo.js
+++ UdemyNotesAlgo.js
@@ -3287,6 +3287,7 @@ class BinarySearchTree {
     constructor(){
         this.root = null;
     }
+    
 }
 
 let tree = new BinarySearchTree();
@@ -3313,8 +3314,76 @@ Starting at the root
             If there is, move to that node and repeat these steps
             If there is not, add that node as the left property 
 
-    
+class Node {
+    constructor(value){
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
 
+class BinarySearchTree {
+    constructor(){
+        this.root = null;
+    }
+    insert(value){
+        var newNode = new Node(value)
+        if(this.root===null) {
+            this.root = newNode
+            return this
+        } else {
+            let current = this.root;
+            while(true){
+                if(value===current.value) return undefined;
+                if(value < current.value){
+                    if(current.left === null){
+                        current.left = newNode;
+                        return this;
+                    } else {
+                        current = current.left;
+                    }
+                } else if(value > current.value) {
+                    if(current.right === null){
+                        current.right = newNode;
+                        return this;
+                    } else {
+                        current = current.right;
+                    }
+                }
+            }
+        }
+    }   
+    find(value){
+        if(this.root === null) return false;
+        let current = this.root,
+             found = false;
+        while(current && !found){
+            if(value < current.value){
+                current = current.left;
+            } else if (value > current.value) {
+                current = current.right;
+            } else {
+                found = true;
+            }
+        }
+        if(!found) return undefined;
+        return current;
+    }
+}
+
+var tree = new BinarySearchTree()
+tree.insert(10)
+tree.insert(5)
+tree.insert(13)
+
+Big O of BST 
+Insertion - O (log n) ...for best and average case
+Searching - O(log n) ...for best and average case 
+
+not guaranteed 
+B/c there are some binary search tree configurations that are very slow
+Let's say a tree becomes a linked list' Would be O(n)
+Fix would be to change the root so values to the rigth and left are larger and smaller than it 
 
 kdljddjk
 
